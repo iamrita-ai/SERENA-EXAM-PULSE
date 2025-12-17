43b8d84d77724aa640469601ea918be389b68688
iamrita-ai <amritanoob2005@gmail.com>
2025-12-17 06:53:53 +0530

    Create database.py

---
+++ src/database.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from .config import config
+
+_client = MongoClient(config.mongo_uri)
+_db = _client[config.mongo_db_name]
+
+
+def get_db():
+    return _db
+
+
+def get_users_collection():
+    return _db["users"]
+
+
+def get_blocked_collection():
+    return _db["blocked_users"]
